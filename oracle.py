2526c371cad3271cfdf982b1123ed923a1a01172
Vardanyan <143796672+varuniv@users.noreply.github.com>
2024-06-13 11:43:43 +0200

    Add files via upload

--- oracle.py
+++ oracle.py
@@ -8,8 +8,9 @@ liste_options=["1 - Ensemble des collaborations entre 2 acteurs",
                "5 - Acteur le plus central du graphe",
                "6 - Distance maximum entre 2 acteurs",
                "7 - Deux acteurs sont proches ou non pour une distance donné",
-               "8 - Dessiner le graphe"
-               "9 - Quitter l'application"
+               "8 - Dessiner le graphe",
+               "9 - List des acteur",
+               "10 - Quitter l'application"
                ]
 
 titre="Veuillez choisir une des options se trouvant ci-dessous"
@@ -19,14 +20,14 @@ titre="Veuillez choisir une des options se trouvant ci-dessous"
 def choix_option():
     valide=False
     while(not valide):
-        choix = input("Entrez le numéro d'une des options entre 1 et 7: ")
+        choix = input("Entrez le numéro d'une des options entre 1 et 10: ")
         if(choix==""):
             print("Vous n'avez pas rentré de numéro")
             print("")
         try:
             opt = int(choix)
         except:
-            print("Vous devez rentrer un numéro qui est compris entre 1 et 7")
+            print("Vous devez rentrer un numéro qui est compris entre 1 et 9")
             continue
         if(opt>len(liste_options) or choix<"1"):
             print("Vous avez entré un numéro non compris dans la liste des options")
@@ -44,20 +45,33 @@ def menu_application(titre, liste_options):
 def continuer_utilisation():
     while True:
         choix = input("Voulez-vous continuer à utiliser l'application? Veuillez répondre par Oui ou par Non: ")
-        if(choix!="Oui" and choix!="Non"):
+        c = ["Oui","OUI","oui","Non","non","NON"]
+        if(choix not in c):
             print("Veuillez répondre par Oui ou par Non")
             continue
         match choix:
             case "Oui":
                 return True
+            case "OUI":
+                return True
+            case "oui":
+                return True
             case "Non":
                 return False
+            case "non":
+                return False
+            case "NON":
+                return False
+            
+
+
+
 
         
 
 # Fonctions appelant les fonctions dans requetes.py concernées par l'option
 
-G=requetes.json_vers_nx("data100.txt")
+G=requetes.json_vers_nx("data_court.txt")
 
 def option_collaborateurs_2_acteurs():
     acteur1=input("Veuillez rentrer un premier acteur")
@@ -112,6 +126,9 @@ def option_est_proche():
 def option_dessiner_graphe():
     return requetes.dessiner_graphe(G)
 
+def option_list_acteur():
+    print(str(G.nodes).replace('[', '').replace(']', '').replace("'", ''))
+
 # Programme principal
 
 def application():
@@ -136,9 +153,13 @@ def application():
             case 8:
                 option_dessiner_graphe()
             case 9:
+                option_list_acteur()
+            case 10:
                 break
         if(not continuer_utilisation()):
             utilisation=False
         print("")
+
+
 
 application()
